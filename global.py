7a16c33c3351c13a4523c15a0e3b46efa7734b61
Ehti-8 <ehtishamibrahim25@gmail.com>
2026-04-14 09:59:03 +0500

    Ehtisham made a Global variable file

---
+++ global.py
@@ -0,0 +1,13 @@
+y = 20 # Global variable
+
+def access_global():
+    print(y) # Can access global variable
+
+access_global() # Output: 20
+
+def modify_global():
+    global y
+    y = 30 # Now modifying the global variable
+
+modify_global()
+print(y) # Output: 30
