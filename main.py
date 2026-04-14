cafb7ed2d50b5a913ffa29f3c3eb59f1dea8ffc7
Ehti-8 <ehtishamibrahim25@gmail.com>
2026-04-14 09:42:38 +0500

    Ehtisham added the global variable code

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 print("hello")
 
 print("branch")
+
+y = 20 # Global variable
+
+def access_global():
+    print(y) # Can access global variable
+
+access_global() # Output: 20
+
+def modify_global():
+    global y
+    y = 30 # Now modifying the global variable
+
+modify_global()
+print(y) # Output: 30
